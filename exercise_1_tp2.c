54ceeae90d51bb83534830f39d97bf1ed95b9284
mustafa <mypcdk1@gmail.com>
2025-11-11 21:37:07 +0100

    robin

---
+++ exercise_1_tp2.c
@@ -0,0 +1,49 @@
+#include< stdio.h >
+#include< string.h >
+#include< stdlib.h >
+char *LoadString(int N){
+    char *name = (char *) malloc(sizeof(char) * N);
+    fgets(name, N, stdin);
+    return name;
+};
+int StringLength(char *str){
+    int a = 0;
+    while( *(str + a) != '\0'){
+        a++;
+    }
+    a++;
+    return a;
+};
+void LoadArray(char *p, char arr[]){
+    int a = StringLength(p);
+    int b = 0;
+    while(b < a){
+        *(arr + b) = *(p + b);
+        b++;
+    }
+};
+void ReverseArray(char arr[], char rev[], int n);
+void DisplayArray(char arr[], int n);
+int SumStringASCII(char *p);
+void ReverseString(char *start, char *end);
+int main() {
+char *str;
+int n;
+printf("Please enter the maximum size of the string:\n");
+scanf("%d", &n);
+getchar();
+str = LoadString(n);
+int len = StringLength(str);
+char arr[len + 1], rev[len + 1];
+LoadArray(str, arr);
+printf("\n Original array: ");
+DisplayArray(arr, len);
+ReverseArray(arr, rev, len);
+printf("\n Reversed array: ");
+DisplayArray(rev, len);
+int sum = SumStringASCII(str);
+printf("\n Sum of ASCII values (recursive): %d", SumStringASCII(str, str + len- 1));
+printf("String reversed recursively: %s\n", str);
+free(str);
+return 0;
+}
